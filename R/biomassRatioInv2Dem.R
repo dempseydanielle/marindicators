b27c848a4208f2fc8ec4c71b079d36c2cff43787
Dempsey <DEMPSEYD@dfo-mpo.ca>
2019-07-16 11:13:29 -0300

    Added loop over spatial scales Values match TARGET

--- R/biomassRatioInv2Dem.R
+++ R/biomassRatioInv2Dem.R
@@ -18,15 +18,34 @@
 #'   \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
 
-biomassRatioInv2Dem <- function(X, syear=yrs[i]) {
-	V <- data.frame(ID=NA,BIOMASS.x=NA,BIOMASS.y=NA,RATIO=NA)
-	if(syear>=1999) {
-	Y <- resourcePotential(X=X,group='GROUNDFISH',metric='BIOMASS')  
-	Z <- resourcePotential(X=X,group='INVERTEBRATES',metric='BIOMASS',yr=syear)  
-	V <- merge(Y,Z,all.x=T,by='ID')
-	V$RATIO <- V$BIOMASS.y/V$BIOMASS.x
-		}
-	V <- V[,c('ID','RATIO')]
+inv2dem <- function(X, years = c(start.year:end.year)) {
+	
+  #V <- data.frame(ID=NA,BIOMASS.x=NA,BIOMASS.y=NA,RATIO=NA)
+  
+  uI = unique(X$ID)                          # extract the spatial scale ID's
+  ind <- NULL                                 # initialize dataframe for storing indicator values
+  
+  for (j in 1:length(uI)){            # loop over all spatal scales
+    
+    X.j = X[X$ID == uI[j], ]          # subset data to spatial scale j
+    
+    for (i in 1:length(years)){                     # loop over each year
+      
+      year.i = years[i]                             # set years.i to current year  
+      X.ij = X.j[X.j$YEAR == year.i, ]              # subset data to include only current year
+      
+      inv.i <- resourcePotential(X = X.ij, group='INVERTEBRATES', metric='BIOMASS')  # calculate biomass of invertebrates
+      dem.i <- resourcePotential(X = X.ij, group='GROUNDFISH', metric='BIOMASS')     # calculate biomass of demersal fish
+    
+      ind.i = inv.i$BIOMASS / dem.i$BIOMASS         # calculate invertebrate to demersal ratio
+      
+      ind.i = data.frame(uI[j], year.i, ind.i)      # create a dataframe with spatial scale ID, year, and indicator value
+       ind = rbind(ind, ind.i)                      # bind ind.i to ind dataframe
+    
+  }
+}
+names(ind) = c("ID", "YEAR", "Invert2Demersal")    # name the ind dataframe
+ind                                                # return vector of indicator values for years c(start.year:end.year) 
 
-	return(V)	
-	}
+}
+
