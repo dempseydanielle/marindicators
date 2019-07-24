e148c4b4fe5ab334f451d110384e9ae21f1fc492
Dempsey <DEMPSEYD@dfo-mpo.ca>
2019-07-24 15:08:17 -0300

    Deleted

--- R/ignore/test.R
+++
@@ -1,9 +0,0 @@
-#' @title test
-#' @description This function is a test.
-#' @param p default is \code{NULL}. This is a parameter.
-#' @family general_use
-#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
-#' @export
-test <- function(p=NULL){
- cat(paste("hi",p))
-}
--- R/ignore/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  localVer = utils::packageDescription('indicators')$Version
-  packageStartupMessage(paste0("Version: ", localVer))
-}
-
-.onLoad <- function(libname, pkgname){
-  options(stringsAsFactors = FALSE)
-}
