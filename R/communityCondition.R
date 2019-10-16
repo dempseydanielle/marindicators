3ca1bf37bd754ec287973d69412e6923f382171a
Dempsey <DEMPSEYD@dfo-mpo.ca>
2019-10-16 09:21:29 -0300

    Made example smaller (removed 2 functional groups)

--- R/communityCondition.R
+++ R/communityCondition.R
@@ -46,8 +46,7 @@
 #'data(X_length)
 #'data(species.table)
 #'data(Length_Weight)
-#'condition.groups <- c("FINFISH", "LBENTHIVORE", "MBENTHIVORE", "PISCIVORE", 
-#'    "PLANKTIVORE", "ZOOPISCIVORE")
+#'condition.groups <- c("FINFISH", "LBENTHIVORE", "PISCIVORE", "PLANKTIVORE")
 #'    
 #'communityCondition(X_length, LenWt.table = Length_Weight, groups = condition.groups, 
 #'    species.table = species.table, years = c(2014:2019))
